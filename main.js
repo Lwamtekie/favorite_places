53628004deb97215f53884cc56fba90e76292d6f
Lwam Tekie <ltekie87@yahoo.com>
2019-04-11 02:49:49 -0500

    wip

--- main.js
+++ main.js
@@ -1,63 +1,57 @@
-let place= [];
+// XHR - XmlHttpRequest
+let Places = [];
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = textToPrint;
 };
 
-// const domStringBuilder = (arrayToPrint) => {
-//   let domString = '';
-//   arrayToPrint.forEach((place) => {
-//     domString += `<h3>${place.name}</h3>`;
-//   })
-//   printToDom('place-container', domString);
-// };
+const domStringBuilder = (arrayToPrint) => {
+  let domString = '';
+  arrayToPrint.forEach((place) => {
+    domString += '<div class="col-4 product">';
+    domString +=   `<div class="card">`;
+    domString +=   `  <div class="card-header">${cityName}</div>`;
+    domString +=   `  <div class="card-header">${cityState}</div>`;
+    domString +=   `  <img src=${place.imageUrl} class="card-img-top" alt="...">`;
+    domString +=   `  <div class="card-body">`;
+    domString += `        <ul class="list-group list-group-flush">`;
+    domString += `          <li class="list-group-item">favorites</li>`;
+    domString += `          <li class="list-group-item">${place.Restaurant}</li>`;
+    domString += `          <li class="list-group-item">${place.bar}</li>`;
+    domString += `          <li class="list-group-item">${place.Hotel}</li>`;
+    domString += `        </ul>`;
+    domString += `      </div>`;
+    domString +=   `    <p class="TouristAttraction">${place.TouristAttraction}</p>`;
+    domString +=   `  </div>`;
+    domString +=   `</div>`;
+    domString +=   `</div>`;
+    domString += '</div>';
 
- 
- const placeBuilder = (arrayToPrint) => {
-    let domString = '';
- 
-    arrayToPrint.forEach((p) => {  
- 
-        domString += `<div class="place">`;
-        domString += `<div class="name"><h4>${p.id}</h4></div>`;
-        domString += `<img src=${p.imageUrl}>`;
-        domString += `<h4>${p.favoriteRestaurant}</h4>`;
-        domString += `<h4>${p.Bar}</h4>`;
-        domString +=` <h4>${p.hotel}</h4>`;
-        domString += `<h4>${p.TouristAttraction}</h4>`;
-        domString += `</div>`;
- 
- 
-    })
- 
- 
-    printToDom('place-container', domString);
- };
- 
+  })
+  printToDom('place-container', domString);
+};
+
 function executeThisCodeAfterFileLoads(){
   const data = JSON.parse(this.responseText);
-  place= data.Places;
-  placeBuilder (place);
-  
-};
+  places = data.place;
+  domStringBuilder(data.places);
+}
 
 function executeThisCodeifXHRFails(){
   console.error('oh shit');
 }
 
-const getPlacesData = () => {
+const getRidesData = () => {
   const myRequest = new XMLHttpRequest();
   myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
   myRequest.addEventListener('error', executeThisCodeifXHRFails);
-  myRequest.open('GET', './db/favorite_places.json');
+  myRequest.open('GET', './db/places.json');
   myRequest.send();
-  console.log(myRequest);
 };
 
 const init = () => {
-  getPlacesData();
-  
+  getPlacesData ();
 };
 
 init();
